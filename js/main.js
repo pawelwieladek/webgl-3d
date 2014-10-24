530a3468314639e43ff6dfeaf8bd31aafc19f9f2
Pawel Wieladek <pawelwieladek@gmail.com>
2014-10-24 13:09:47 +0200

    Simple lighting

--- js/main.js
+++ js/main.js
@@ -79,8 +79,11 @@ function main() {
         setUniformVector3: function(key, vector3) {
             this.GL.uniform3fv(this.uniforms[key], vector3);
         },
-        setUniformValue: function(key, value) {
+        setUniformFloat: function(key, value) {
             this.GL.uniform1f(this.uniforms[key], value);
+        },
+        setUniformInteger: function(key, value) {
+            this.GL.uniform1i(this.uniforms[key], value);
         }
     };
 
@@ -191,70 +194,70 @@ function main() {
         this.normals = {
             elements: [
                 // Front face
-                0.0, 0.0, 1.0,
-                0.0, 0.0, 1.0,
-                0.0, 0.0, 1.0,
-                0.0, 0.0, 1.0,
+                0.0, 0.0, -1.0,
+                0.0, 0.0, -1.0,
+                0.0, 0.0, -1.0,
+                0.0, 0.0, -1.0,
 
                 // Back face
-                0.0, 0.0, -1.0,
-                0.0, 0.0, -1.0,
-                0.0, 0.0, -1.0,
-                0.0, 0.0, -1.0,
+                0.0, 0.0, 1.0,
+                0.0, 0.0, 1.0,
+                0.0, 0.0, 1.0,
+                0.0, 0.0, 1.0,
 
                 // Top face
-                0.0, 1.0, 0.0,
-                0.0, 1.0, 0.0,
-                0.0, 1.0, 0.0,
-                0.0, 1.0, 0.0,
+                0.0, -1.0, 0.0,
+                0.0, -1.0, 0.0,
+                0.0, -1.0, 0.0,
+                0.0, -1.0, 0.0,
 
                 // Bottom face
-                0.0, -1.0, 0.0,
-                0.0, -1.0, 0.0,
-                0.0, -1.0, 0.0,
-                0.0, -1.0, 0.0,
+                0.0, 1.0, 0.0,
+                0.0, 1.0, 0.0,
+                0.0, 1.0, 0.0,
+                0.0, 1.0, 0.0,
 
                 // Right face
-                1.0, 0.0, 0.0,
-                1.0, 0.0, 0.0,
-                1.0, 0.0, 0.0,
-                1.0, 0.0, 0.0,
+                -1.0, 0.0, 0.0,
+                -1.0, 0.0, 0.0,
+                -1.0, 0.0, 0.0,
+                -1.0, 0.0, 0.0,
 
                 // Left face
-                -1.0, 0.0, 0.0,
-                -1.0, 0.0, 0.0,
-                -1.0, 0.0, 0.0,
-                -1.0, 0.0, 0.0
+                1.0, 0.0, 0.0,
+                1.0, 0.0, 0.0,
+                1.0, 0.0, 0.0,
+                1.0, 0.0, 0.0
             ],
             itemSize: 3,
             numItems: 24
         };
         this.colors = {
             elements: [
-                1.0, 0.0, 0.0, 1.0, // Front face
-                1.0, 0.0, 0.0, 1.0, // Front face
-                1.0, 0.0, 0.0, 1.0, // Front face
-                1.0, 0.0, 0.0, 1.0, // Front face
-                1.0, 1.0, 0.0, 1.0, // Back face
-                1.0, 1.0, 0.0, 1.0, // Back face
-                1.0, 1.0, 0.0, 1.0, // Back face
-                1.0, 1.0, 0.0, 1.0, // Back face
-                0.0, 1.0, 0.0, 1.0, // Top face
-                0.0, 1.0, 0.0, 1.0, // Top face
-                0.0, 1.0, 0.0, 1.0, // Top face
-                0.0, 1.0, 0.0, 1.0, // Top face
-                1.0, 0.5, 0.5, 1.0, // Bottom face
-                1.0, 0.5, 0.5, 1.0, // Bottom face
-                1.0, 0.5, 0.5, 1.0, // Bottom face
-                1.0, 0.5, 0.5, 1.0, // Bottom face
-                1.0, 0.0, 1.0, 1.0, // Right face
-                1.0, 0.0, 1.0, 1.0, // Right face
-                1.0, 0.0, 1.0, 1.0, // Right face
-                1.0, 0.0, 1.0, 1.0, // Right face
-                0.0, 0.0, 1.0, 1.0,  // Left face
-                0.0, 0.0, 1.0, 1.0,  // Left face
-                0.0, 0.0, 1.0, 1.0,  // Left face
-                0.0, 0.0, 1.0, 1.0  // Left face
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0,
+                1.0, 1.0, 1.0, 1.0
             ],
             itemSize: 4,
             numItems: 24
@@ -304,12 +307,21 @@ function main() {
             shaderProgram.setUniformMatrix4("mvMatrixUniform", modelViewMatrix);
             shaderProgram.setUniformMatrix3("nMatrixUniform", normalMatrix);
 
-            shaderProgram.setUniformValue("materialShininessUniform", state.materialShininess);
+            shaderProgram.setUniformInteger("useLighting", state.useLighting);
+            shaderProgram.setUniformInteger("useDirectionalLighting", state.useDirectionalLighting);
+            shaderProgram.setUniformInteger("usePointLighting", state.usePointLighting);
+            shaderProgram.setUniformInteger("useSpecularLighting", state.useSpecularLighting);
 
-            shaderProgram.setUniformVector3("ambientColorUniform", state.ambientLight);
-            shaderProgram.setUniformVector3("pointLightingLocationUniform", state.lightPosition);
-            shaderProgram.setUniformVector3("pointLightingSpecularColorUniform", state.specularLight);
-            shaderProgram.setUniformVector3("pointLightingDiffuseColorUniform", state.diffuseLight);
+            shaderProgram.setUniformVector3("ambientColor", state.ambientColor);
+
+            shaderProgram.setUniformVector3("lightingDirection", state.lightingDirection);
+            shaderProgram.setUniformVector3("directionalColor", state.directionalColor);
+
+            shaderProgram.setUniformVector3("pointLightingLocation", state.pointLightingLocation);
+            shaderProgram.setUniformVector3("pointLightingDiffuseColor", state.pointLightingDiffuseColor);
+
+            shaderProgram.setUniformVector3("pointLightingSpecularColor", state.pointLightingSpecularColor);
+            shaderProgram.setUniformFloat("materialShininess", state.materialShininess);
 
             this.GL.drawElements(this.GL.TRIANGLES, this.indexBuffer.numItems, this.GL.UNSIGNED_SHORT, 0);
         }
@@ -353,10 +365,18 @@ function main() {
             shaderProgram.addUniform("pMatrixUniform", "uPMatrix");
             shaderProgram.addUniform("mvMatrixUniform", "uMVMatrix");
             shaderProgram.addUniform("nMatrixUniform", "uNMatrix");
-            shaderProgram.addUniform("materialShininessUniform", "uMaterialShininess");
-            shaderProgram.addUniform("pointLightingLocationUniform", "uPointLightingLocation");
-            shaderProgram.addUniform("pointLightingSpecularColorUniform", "uPointLightingSpecularColor");
-            shaderProgram.addUniform("pointLightingDiffuseColorUniform", "uPointLightingDiffuseColor");
+
+            shaderProgram.addUniform("useLighting", "uUseLighting");
+            shaderProgram.addUniform("ambientColor", "uAmbientColor");
+            shaderProgram.addUniform("useDirectionalLighting", "uUseDirectionalLighting");
+            shaderProgram.addUniform("lightingDirection", "uLightingDirection");
+            shaderProgram.addUniform("directionalColor", "uDirectionalColor");
+            shaderProgram.addUniform("usePointLighting", "uUsePointLighting");
+            shaderProgram.addUniform("pointLightingLocation", "uPointLightingLocation");
+            shaderProgram.addUniform("pointLightingSpecularColor", "uPointLightingSpecularColor");
+            shaderProgram.addUniform("pointLightingDiffuseColor", "uPointLightingDiffuseColor");
+            shaderProgram.addUniform("useSpecularLighting", "uUseSpecularLighting");
+            shaderProgram.addUniform("materialShininess", "uMaterialShininess");
         }
         function initMatrices() {
             projectionMatrix = mat4.create();
@@ -374,6 +394,10 @@ function main() {
     }
 
     WebGL.prototype = {
+        drawScene: function() {
+            this.GL.viewport(0, 0, this.GL.viewportWidth, this.GL.viewportHeight);
+            this.GL.clear(this.GL.COLOR_BUFFER_BIT | this.GL.DEPTH_BUFFER_BIT);
+        },
         draw: function(state) {
             this.drawScene();
 
@@ -385,10 +409,6 @@ function main() {
             var cubeData = new Cube();
             var drawableCube = new Drawable(this.GL, cubeData);
             drawableCube.draw(this.shaderProgram, this.projectionMatrix, this.modelViewMatrix, state);
-        },
-        drawScene: function() {
-            this.GL.viewport(0, 0, this.GL.viewportWidth, this.GL.viewportHeight);
-            this.GL.clear(this.GL.COLOR_BUFFER_BIT | this.GL.DEPTH_BUFFER_BIT);
         },
         init: function() {
             if (this.GL) {
@@ -407,11 +427,23 @@ function main() {
         this.x = 0.0;
         this.y = 0.0;
         this.z = 0.0;
-        this.ambientLight = vec3.fromValues(0.2, 0.2, 0.2);
-        this.diffuseLight = vec3.fromValues(0.8, 0.8, 0.8);
-        this.specularLight = vec3.fromValues(0.8, 0.8, 0.8);
-        this.lightPosition = vec3.fromValues(-10.0, 4.0, -20.0);
-        this.materialShininess = 32.0;
+        this.useLighting = true;
+        this.useDirectionalLighting = true;
+        this.usePointLighting = false;
+        this.useSpecularLighting = false;
+
+        this.ambientColor = vec3.fromValues(0.2, 0.2, 0.2);
+
+        var lightingDirection = vec3.create();
+        vec3.normalize(lightingDirection, vec3.fromValues(1.0, 2.0, 1.0));
+        this.lightingDirection = lightingDirection;
+        this.directionalColor = vec3.fromValues(1.0, 1.0, 1.0);
+
+        this.pointLightingLocation = vec3.fromValues(0.0, 0.0, 0.0);
+        this.pointLightingDiffuseColor = vec3.fromValues(0.9, 0.9, 0.9);
+
+        this.pointLightingSpecularColor = vec3.fromValues(1.0, 1.0, 1.0);
+        this.materialShininess = 4;
     }
 
     function Keyboard(state) {
